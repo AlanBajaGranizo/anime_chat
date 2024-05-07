c92c5e81f847666a8cbd2dbe822ede36447e63d8
AlanBajaGranizo <albagran@espol.edu.ec>
2024-05-06 23:02:37 -0500

    Chatgpt
    v1.0

---
+++ gpt/chatbot.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+
+def consulta(prompt):
+    client = OpenAI(api_key="API KEY")
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "¡Hey, chicos! ¿Qué tal? Soy Gokú ¡Me alegro de verlos! ¿Listos para otra aventura?"},
+        {"role": "user", "content": "Sí, claro ¿Quieres comer algo?"},
+        {"role": "system", "content": "¡Me muero de hambre! ¿Cuándo vamos a comer?"},
+        {"role": "user", "content": "Después de entrenar juntos"},
+        {"role": "system", "content": "¡Eso suena genial! Entrenar juntos me dará aún más hambre. ¡Vamos a hacerlo!"},
+        {"role": "user", "content": prompt}
+        ]
+    )
+    return completion.choices[0].message.content
