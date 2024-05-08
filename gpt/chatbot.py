51ea2fac47bb68af940158221bd972e480ea4d21
porrasmdz <aporrasmdz@gmail.com>
2024-05-07 23:49:43 -0500

    finished chatbot readme

--- gpt/chatbot.py
+++ gpt/chatbot.py
@@ -1,16 +1,21 @@
 from openai import OpenAI
 
+
 def consulta(prompt):
-    client = OpenAI(api_key="API KEY")
+    client = OpenAI(
+        api_key="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")
     completion = client.chat.completions.create(
-    model="ft:gpt-3.5-turbo-0125:personal:goku2:96rKiPC5",
-    messages=[
-        {"role": "system", "content": "¡Hey, chicos! ¿Qué tal? Soy Gokú ¡Me alegro de verlos! ¿Listos para otra aventura?"},
-        {"role": "user", "content": "Sí, claro ¿Quieres comer algo?"},
-        {"role": "system", "content": "¡Me muero de hambre! ¿Cuándo vamos a comer?"},
-        {"role": "user", "content": "Después de entrenar juntos"},
-        {"role": "system", "content": "¡Eso suena genial! Entrenar juntos me dará aún más hambre. ¡Vamos a hacerlo!"},
-        {"role": "user", "content": prompt}
+        model="ft:gpt-3.5-turbo-0125:personal:goku2:96rKiPC5",
+        messages=[
+            {"role": "system", "content": "¡Hey, chicos! ¿Qué tal? Soy Gokú ¡Me alegro de verlos! ¿Listos para otra aventura?"},
+            {"role": "user", "content": "Sí, claro ¿Quieres comer algo?"},
+            {"role": "system", "content": "¡Me muero de hambre! ¿Cuándo vamos a comer?"},
+            {"role": "user", "content": "Después de entrenar juntos"},
+            {"role": "system", "content": "¡Eso suena genial! Entrenar juntos me dará aún más hambre. ¡Vamos a hacerlo!"},
+            {"role": "user", "content": prompt}
         ]
     )
-    return completion.choices[0].message.content
+    result = completion.choices[0].message.content
+    print("RESULT", result)
+
+consulta("Como se que de verdad eres goku? Muestrame una tecnica sayayin")
